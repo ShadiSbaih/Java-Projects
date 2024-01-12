65804ec20750e27bcfe73d1b55dfcca2e41956fb
Shad1Sba1h <156319075+Shad1Sba1h@users.noreply.github.com>
2024-01-12 15:53:27 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       /* String str = "Programing";
+        String revString = "";
+        for (int i = 0; i < str.length(); i++) {
+            revString = str.charAt(i) + revString; }
+
+        System.out.println(revString);
+        in.close(); */
+     
+   
+    
+    
+
+    }
+}
